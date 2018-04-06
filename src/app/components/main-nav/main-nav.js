b1172c6725cf816e8ee62c4d4c338b3fc9b31eae
Joeffison Andrade <joeffison@gmail.com>
2018-04-06 19:50:46 -0300

    Adding responsive dropdown options to main-nav (works on iPhone X in both orientations)

--- src/app/components/main-nav/main-nav.js
+++ src/app/components/main-nav/main-nav.js
@@ -1,5 +1,26 @@
 angular
   .module('app')
   .component('mainNav', {
-    templateUrl: 'app/components/main-nav/main-nav.html'
+    templateUrl: 'app/components/main-nav/main-nav.html',
+    controller: ['$document', MainNav]
   });
+
+function MainNav($document) {
+  $document.ready(() => {
+    angular.element($document.querySelectorAll('li.dropdown')).on('click', event => {
+      event.preventDefault();
+      angular.element(event.target.parentElement).toggleClass('active');
+      event.stopPropagation();
+    });
+
+    // Hide dropdown if clicked somewhere 'outside'
+    $document.on('click', event => {
+      closeAllNavMenus();
+      return $document.off('click', event);
+    });
+  });
+
+  function closeAllNavMenus() {
+    angular.element($document.querySelectorAll('li.dropdown')).removeClass('active');
+  }
+}
